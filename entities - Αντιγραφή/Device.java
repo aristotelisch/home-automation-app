7c23c35188e6d38cfe420ae9e950215f78039a95
GregoryKarles <44946202+GregoryKarles@users.noreply.github.com>
2018-11-25 17:04:42 +0200

    Add files via upload

---
+++ entities - Αντιγραφή/Device.java
@@ -0,0 +1,72 @@
+package eu.codingschool.black.homeautomation.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+
+@Entity
+public class Device {
+  @Id
+  @GeneratedValue
+  private long id;
+
+  private String name;
+  private String type;
+  private boolean status;
+  private String information;
+  @ManyToOne
+  @JoinColumn(name = "room")
+  private Room room;
+
+
+
+
+
+  public long getId () {
+    return id;
+  }
+
+  public void setId (long id) {
+    this.id = id;
+  }
+
+  public String getName () {
+    return name;
+  }
+
+  public void setName (String name) {
+    this.name = name;
+  }
+
+  public String getType () {
+    return type;
+  }
+
+  public void setType (String type) {
+    this.type = type;
+  }
+
+  public boolean isStatus () {
+    return status;
+  }
+
+  public void setStatus (boolean status) {
+    this.status = status;
+  }
+
+  public String getInformation () {
+    return information;
+  }
+
+  public void setInformation (String information) {
+    this.information = information;
+  }
+
+  @Override
+  public String toString() {
+    return this.id + " " +this.name;
+  }
+}
