fb300424e6467be23b988d4254fa8e1b06e92728
mariliatmf <mariliatrivyza@hotmail.com>
2018-11-27 21:28:31 +0200

    Update correct frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DevicesComponent} from './devices/devices.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { KitchenDevicesComponent } from './kitchen-devices/kitchen-devices.component';
 
 const routes: Routes = [
+
+  { path: '', redirectTo: '/login',  pathMatch: 'full'},
+  { path: 'login', component: LoginPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'home/kitchen', component: KitchenDevicesComponent },
+
+  //
   { path: 'devices', component: DevicesComponent, pathMatch: 'full' }
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DevicesComponent } from './devices/devices.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomePageComponent } from './home-page/home-page.component';
+import { KitchenDevicesComponent } from './kitchen-devices/kitchen-devices.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DevicesComponent
+    DevicesComponent,
+    HomePageComponent,
+    KitchenDevicesComponent,
+    LoginPageComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
