006afed6f3c7551d628a2ddfc0ec7660ac4aaacd
mariliatmf <mariliatrivyza@hotmail.com>
2018-11-29 16:07:13 +0200

    Create table for devices with update, delete and add buttons

---
+++ frontend/src/app/admin-tables/admin-tables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-tables',
+  templateUrl: './admin-tables.component.html',
+  styleUrls: ['./admin-tables.component.css']
+})
+export class AdminTablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
