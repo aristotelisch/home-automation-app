a8bb9416558e5a9b6c79101f4d5f662ca6b76368
Aristotelis Christou <telischristou@gmail.com>
2018-12-02 13:53:56 +0200

    Frontend: Add theme

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DevicesComponent } from './devices/devices.component';
@@ -9,6 +10,12 @@ import { RoomsComponent } from './rooms/rooms.component';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
 import { RoomComponent } from './room/room.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import {FormsModule} from '@angular/forms';
+import {AppService} from './app.service';
 
 @NgModule({
   declarations: [
@@ -19,13 +26,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     // DeviceComponent TODO
     RoomsComponent,
     RoomComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    BreadcrumbComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
