c477761822f1505ba49176a994cd43de5f87589e
Aristotelis Christou <telischristou@gmail.com>
2018-12-08 17:07:04 +0200

    Add more device associations

--- backend/src/main/java/eu/codingschool/black/homeautomation/HomeAutomationApplication.java
+++ backend/src/main/java/eu/codingschool/black/homeautomation/HomeAutomationApplication.java
@@ -13,9 +13,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 @SpringBootApplication
@@ -60,10 +61,7 @@ public class HomeAutomationApplication implements CommandLineRunner {
 		Room lRoom6 = new Room ("Yard", "yard.jpg");
 		roomService.save (lRoom6);
 
-		deviceService.save (new Device ("Living Room Light 1", "onoff", true, "Living Room Light is Switched ON", lRoom1));
-		deviceService.save (new Device ("Living Room Light 2", "onoff", true, "Living Room Light is Switched ON", lRoom1));
-		deviceService.save (new Device ("Bedroom Light 1", "onoff", true, "Bedroom Light is Switched ON", lRoom2));
-		deviceService.save (new Device ("Bedroom Light 2", "onoff", true, "Bedroom Light is Switched ON", lRoom2));
+
 		deviceService.save (new Device ("Kitchen Light", "onoff", true, "Kitchen Light is Switched ON", lRoom3));
 		deviceService.save (new Device ("Oven", "onoff", true, "Oven is turned ON", lRoom3));
 		deviceService.save (new Device ("Refrigerator", "onoff", true, "Refrigerator is turned ON", lRoom3));
@@ -71,30 +69,53 @@ public class HomeAutomationApplication implements CommandLineRunner {
 		deviceService.save (new Device ("Jacuzzi", "onoff", true, "Jacuzzi is warming", lRoom4));
 		deviceService.save (new Device ("Garage Light", "onoff", true, "Garage Light is switched ON", lRoom5));
 		deviceService.save (new Device ("Garage Door", "onoff", true, "Garage Door is opened", lRoom5));
-		deviceService.save (new Device ("Yard Light", "onoff", true, "Yard Light is turned ON", lRoom6);
-		deviceService.save (new Device ("Fountain", "onoff", true, "Fountain is making everyone wet", lRoom6);
+		deviceService.save (new Device ("Yard Light", "onoff", true, "Yard Light is turned ON", lRoom6));
+		deviceService.save (new Device ("Fountain", "onoff", true, "Fountain is making everyone wet", lRoom6));
 
 
+		Device device1 = new Device ("Living Room Light 1", "onoff", true, "Living Room Light is Switched ON", lRoom1);
+		deviceService.save (device1);
 
+		Device device2 = new Device ("Living Room Light 2", "onoff", true, "Living Room Light is Switched ON", lRoom1);
+		deviceService.save (device2);
+
+		Device device3 = new Device ("Living Room Light 3", "onoff", true, "Living Room Light is Switched ON", lRoom1);
+		deviceService.save (device3);
+
+    Device device4 = new Device ("Bedroom Light 1", "onoff", true, "Bedroom Light is Switched ON", lRoom2);
+    deviceService.save(device4);
+
+    Device device5 = new Device ("Bedroom Light 2", "onoff", true, "Bedroom Light is Switched ON", lRoom2);
+		deviceService.save (device5);
 
 		PersonRole userRole = new PersonRole ("ROLE_USER");
 		PersonRole adminRole = new PersonRole ("ROLE_ADMIN");
 		personRoleRepository.save(userRole);
 		personRoleRepository.save(adminRole);
 
+		Set<Device> simpleUserDevices = new HashSet<>();
+		simpleUserDevices.add(device1);
+		simpleUserDevices.add(device2);
+		simpleUserDevices.add(device4);
+
+		Set<Device> adminUserDevices = new HashSet<>();
+		adminUserDevices.add(device1);
+		adminUserDevices.add(device3);
+		adminUserDevices.add(device5);
+
 		Person simpleUser = new Person();
 		simpleUser.setUsername ("user");
 		simpleUser.setPassword ("password");
 		simpleUser.setPersonrole (userRole);
+		simpleUser.setDevice (simpleUserDevices);
 		personService.save (simpleUser);
 
 		Person adminUser = new Person();
 		adminUser.setUsername ("admin");
 		adminUser.setPassword ("password");
 		adminUser.setPersonrole (adminRole);
+		adminUser.setDevice (adminUserDevices);
 		personService.save (adminUser);
-
-
 
 		roomService.findAll ().forEach (room -> System.out.println (room.getName ()));
 		deviceService.findAll ().forEach (device -> System.out.println (device.getName ()));
--- frontend/src/app/services/device.service.ts
+++ frontend/src/app/services/device.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Device} from '../models/Device';
+
 // import {mock_devices} from '../models/mock-devices';
 import {Observable, of} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
@@ -39,4 +40,3 @@ export class DeviceService {
     return this.http.put<Device[]>(this.devicesUrl, device, { headers: this.app.authHeaders } );
   }
 }
-
